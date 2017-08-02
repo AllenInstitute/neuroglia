8140cec81ba585e548f1ec8925ffd98098c630b3
Justin Kiggins <justink@alleninstitute.org>
2017-08-01 18:42:05 -0700

    fix imports

--- neuroglia/__init__.py
+++ neuroglia/__init__.py
@@ -4,6 +4,7 @@ __version__ = '0.1.0'
 __author__ = 'Justin Kiggins <justink@alleninstitute.org>'
 __all__ = []
 
-from spike import *
-from trace import *
-from tensor import *
+from .spike import *
+from .trace import *
+from .tensor import *
+from .nwb import *
