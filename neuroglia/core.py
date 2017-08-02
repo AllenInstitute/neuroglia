4383e55347f62aa24a13dc11b209bae32794c092
Justin Kiggins <justink@alleninstitute.org>
2017-08-01 18:42:28 -0700

    do validation in base tensorizer

--- neuroglia/core.py
+++ neuroglia/core.py
@@ -3,6 +3,8 @@ from sklearn.base import TransformerMixin
 
 class BaseTensorizer(TransformerMixin):
     """docstring for SpikeTensorizer."""
-    def __init__(self, events):
+    def __init__(self, events, window):
         super(BaseTensorizer, self).__init__()
         self.events = events
+        assert window[0]<window[1], 'window[0] must be less than window[1]'
+        self.window = window
