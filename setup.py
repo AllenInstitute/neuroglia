dd2082fad81ade72bbceb061f326085cb1047303
Justin Kiggins <justink@alleninstitute.org>
2017-08-01 18:41:43 -0700

    fix README read

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author_email="justink@alleninstitute.org",
 
     description="pipelines for transformations for neurophysiology data",
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
 
     packages=setuptools.find_packages(),
 
