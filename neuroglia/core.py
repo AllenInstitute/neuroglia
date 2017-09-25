7ad63dfd64cb15bc43a4e07cc482daaa8a91a116
Justin Kiggins <justink@alleninstitute.org>
2017-09-25 16:00:11 -0700

    modularize

--- neuroglia/core.py
+++ neuroglia/core.py
@@ -2,6 +2,18 @@ import numpy as np
 import xarray as xr
 from sklearn.base import TransformerMixin
 
+def events_to_xr_dim(events,dim='event'):
+    # builds the event dataframe into coords for xarray
+    coords = events.to_dict(orient='list')
+    coords = {k:(dim,v) for k,v in coords.items()}
+    # define a DataArray that will describe the event dimension
+    return xr.DataArray(
+        events.index,
+        name=dim,
+        dims=[dim],
+        coords=coords,
+    )
+
 class BaseTensorizer(TransformerMixin):
     """docstring for SpikeTensorizer."""
     def __init__(self, events, bins, range):
@@ -9,22 +21,7 @@ class BaseTensorizer(TransformerMixin):
         self.events = events
         _, bins = np.histogram([],bins,range)
         self.bins = bins
-
-        # builds the event dataframe into coords for xarray
-        coords = (
-            self.events
-            .reset_index()
-            .to_dict(orient='list')
-            )
-        coords = {k:('event',v) for k,v in coords.items()}
-
-        # define a DataArray that will describe the event dimension
-        self.concat_dim = xr.DataArray(
-            self.events.index,
-            name='event',
-            dims=['event'],
-            coords=coords,
-            )
+        self.concat_dim = events_to_xr_dim(self.events)
 
         # TODO: use property decorator to keep self.events and self.concat_dim in sync
         
