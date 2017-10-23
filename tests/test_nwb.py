a15fcd6d0ec1f8b71e5b833a080a934407716d5d
nicholasc <nicholasc@alleninstitute.org>
2017-10-23 23:38:39 +0000

    testing fit and fit_transform

--- tests/test_nwb.py
+++ tests/test_nwb.py
@@ -4,10 +4,12 @@ import pandas.util.testing as pdt
 
 
 def test_spike_tablizer():
-    compare = ng.nwb.SpikeTablizer().transform({'a':[1.1,2,3], 'b':[1,2.5,6]})
+    compare_list = [ng.nwb.SpikeTablizer().transform({'a':[1.1,2,3], 'b':[1,2.5,6]}),
+                    ng.nwb.SpikeTablizer().fit_transform({'a': [1.1, 2, 3], 'b': [1, 2.5, 6]})]
     base = pd.DataFrame({'neuron': ['b', 'a', 'a', 'b', 'a', 'b'],
                          'time': [1.0, 1.1, 2.0, 2.5, 3.0, 6.0]}).set_index('time')
-    pdt.assert_frame_equal(base, compare)
+    for compare in compare_list:
+        pdt.assert_frame_equal(base, compare)
 
 if __name__ == "__main__":
     test_spike_tablizer()
