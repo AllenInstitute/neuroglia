861cda229b9ef34ab2067fd21bbef566e3d958f9
Michael Oliver <michael.d.oliver@gmail.com>
2017-10-23 15:33:49 -0700

    Median filter detrending

--- neuroglia/calcium.py
+++ neuroglia/calcium.py
@@ -1,6 +1,39 @@
 import numpy as np
 from sklearn.base import TransformerMixin
 from oasis.functions import deconvolve
+from scipy.signal import medfilt, savgol_filter
+
+class MedianFilterDetrend(TransformerMixin):
+    """
+    Median filter detrending
+    """
+    def __init__(self,
+        window=101,
+        peak_std_threshold=4)
+
+        self.window = window
+        self.peak_std_threshold = peak_std_threshold
+
+    def robust_std(self, x):
+        '''
+        Robust estimate of std
+        '''
+        MAD = np.median(np.abs(x - np.median(x)))
+        return 1.4826*MAD
+
+    def fit(self, X, y=None):
+        self.fit_params = {}
+        return self
+
+    def transform(self,X):
+        X_new = X.copy()
+        for col in X.columns:
+            mf = medfilt(X[col].values.astype(np.double), self.window)
+            mf = np.minimum(mf, self.peak_std_threshold * self.robust_std(mf))
+            X_new[col] = X[col].values.astype(np.double) - mf
+
+        return X_new
+
 
 class OASISInferer(TransformerMixin):
     """docstring for OASISInferer."""
