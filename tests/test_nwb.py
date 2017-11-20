69ca41024d5f986819a403bd2ede0778e3cb8765
Justin Kiggins <justink@alleninstitute.org>
2017-11-20 12:50:50 -0800

    refactor tests for spike tablizer

--- tests/test_nwb.py
+++ tests/test_nwb.py
@@ -4,11 +4,21 @@ import pandas.util.testing as pdt
 
 
 def test_spike_tablizer():
-    compare_list = [ng.nwb.SpikeTablizer().transform({'a':[1.1,2,3], 'b':[1,2.5,6]}),
-                    ng.nwb.SpikeTablizer().fit_transform({'a': [1.1, 2, 3], 'b': [1, 2.5, 6]})]
+    spike_dict = {
+        'a': [1.1,2,3],
+        'b': [1,2.5,6],
+        }
+
+    compare_list = [
+        ng.nwb.SpikeTablizer().transform(spike_dict).reset_index(drop=True),
+        ng.nwb.SpikeTablizer().fit_transform(spike_dict).reset_index(drop=True),
+        ]
+
     base = pd.DataFrame({'neuron': ['b', 'a', 'a', 'b', 'a', 'b'],
-                         'time': [1.0, 1.1, 2.0, 2.5, 3.0, 6.0]}).set_index('time')
+                         'time': [1.0, 1.1, 2.0, 2.5, 3.0, 6.0]})
     for compare in compare_list:
+        print(base)
+        print(compare)
         pdt.assert_frame_equal(base, compare)
 
 if __name__ == "__main__":
