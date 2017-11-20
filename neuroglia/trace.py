51d1796cfcc6bbd99cf35f0c3a293079e04d89cb
Justin Kiggins <justink@alleninstitute.org>
2017-11-20 12:37:55 -0800

    docstrings for Binarizer

--- neuroglia/trace.py
+++ neuroglia/trace.py
@@ -4,7 +4,32 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.preprocessing import binarize
 
 class Binarizer(BaseEstimator, TransformerMixin):
-    """docstring for scikit learn Binarizer
+    """Binarize data (set feature values to 0 or 1) according to a threshold
+
+    This transformer is a DataFram-friendly alternative to
+    sklearn.preprocessing.Binarizer
+
+    Values greater than the threshold map to 1, while values less than
+    or equal to the threshold map to 0. With the default threshold of 0,
+    only positive values map to 1.
+
+    Parameters
+    ----------
+    threshold : float, optional (0.0 by default)
+        Feature values below or equal to this are replaced by 0, above it by 1.
+        Threshold may not be less than 0 for operations on sparse matrices.
+    copy : boolean, optional, default True
+        set to False to perform inplace binarization and avoid a copy (if
+        the input is already a numpy array or a scipy.sparse CSR matrix).
+
+    Notes
+    -----
+
+    If the input is a sparse matrix, only the non-zero values are subject
+    to update by the Binarizer class.
+
+    This estimator is stateless (besides constructor parameters), the
+    fit method does nothing but is useful when used in a pipeline.
     """
 
     def __init__(self, threshold=0.0, copy=True):
@@ -12,9 +37,30 @@ class Binarizer(BaseEstimator, TransformerMixin):
         self.copy = copy
 
     def fit(self, X, y=None):
+        """Do nothing and return the estimator unchanged
+
+        This method is here to implement the scikit-learn API and work in
+        scikit-learn pipelines.
+
+        Parameters
+        ----------
+        X : array-like
+
+        Returns
+        -------
+        self
+
+        """
         return self
 
     def transform(self, X):
+        """Binarize each element of X
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape [n_samples, n_features]
+            The data to binarize, element by element.
+        """
         df = True
         try:
             index = X.index
