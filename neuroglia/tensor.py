d04ad7c08eeba1a91a146b68a877731e0a17b50c
Justin Kiggins <justink@alleninstitute.org>
2017-11-20 13:25:53 -0800

    docs & small API change

--- neuroglia/tensor.py
+++ neuroglia/tensor.py
@@ -3,20 +3,45 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 
 class ResponseReducer(BaseEstimator,TransformerMixin):
-    """docstring for Annotator."""
-    def __init__(self, method='mean', dim='sample_times'):
-        super(ResponseReducer, self).__init__()
+    """Reduces a response tensor by performing a function along one dimension
 
-        if method == 'mean':
-            self.method = np.mean
-        elif method == 'max':
-            self.method = np.max
-        else:
-            self.method = method
+
+    Notes
+    -----
+
+    This estimator is stateless (besides constructor parameters), the
+    fit method does nothing but is useful when used in a pipeline.
+    """
+    def __init__(self, func, dim='sample_times'):
+        self.func = func
         self.dim = dim
 
     def fit(self, X, y=None):
+        """Do nothing and return the estimator unchanged
+
+        This method is here to implement the scikit-learn API and work in
+        scikit-learn pipelines.
+
+        Parameters
+        ----------
+        X : xarray.DataArray in `response tensor` structre ['events','sample_times','neurons']
+
+        Returns
+        -------
+        self
+
+        """
         return self
 
     def transform(self, X):
-        return X.reduce(self.method,dim=self.dim)
+        """Reduces a response tensor by performing a function along one dimension
+
+        Parameters
+        ----------
+        X : xarray.DataArray in `response tensor` structre ['events','sample_times','neurons']
+
+        Returns
+        -------
+        Xt : xarray.DataArray with remaining dimensions
+        """
+        return X.reduce(self.func,dim=self.dim)
