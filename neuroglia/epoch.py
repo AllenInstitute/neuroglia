ea3e02b32e2ad14e17be9cfb1b0f62fb9e159e8c
Justin Kiggins <justink@alleninstitute.org>
2017-11-21 13:45:56 -0800

    rename & add docstrings

--- neuroglia/epoch.py
+++ neuroglia/epoch.py
@@ -4,21 +4,60 @@ import xarray as xr
 
 from sklearn.base import BaseEstimator,TransformerMixin
 
-class EpochReducer(BaseEstimator,TransformerMixin):
-    """docstring for EpochReducer."""
-    def __init__(self, traces, method='mean'):
+class EpochTraceReducer(BaseEstimator,TransformerMixin):
+    """Take event-aligned samples of traces from a population of neurons.
+
+    Traces are sampled relative to the event time. There is no enforced
+    constraint that the times of events or sample_times relative to the events
+    need to align to trace sample times. Rather, samples are interpolated from
+    the values in the traces DataFrame.
+
+    Parameters
+    ----------
+    traces : pandas DataFrame with 'time' as the index and neuron IDs in columns
+        The traces that will be sampled from when the transform method is called
+    func : function
+        Function that will be applied to trace samples within epochs
+
+    Notes
+    -----
+
+    This estimator is stateless (besides constructor parameters), the
+    fit method does nothing but is useful when used in a pipeline.
+    """
+    def __init__(self, traces, func):
         self.traces = traces
-        self.method = method
+        self.func = func
 
     def fit(self, X, y=None):
-        if self.method == 'mean':
-            self.method = np.mean
-        elif self.method == 'max':
-            self.method = np.max
+        """Do nothing and return the estimator unchanged
+
+        This method is here to implement the scikit-learn API and work in
+        scikit-learn pipelines.
+
+        Parameters
+        ----------
+        X : array-like
+
+        Returns
+        -------
+        self
+
+        """
 
         return self
 
     def transform(self, X):
+        """Reduce traces around each event
+
+        Parameters
+        ----------
+        X : pandas.DataFrame with a columns ['time','duration']
+
+        Returns
+        -------
+        Xt : xarray.DataArray with columns ['event','neuron']
+        """
 
         # define a local function that will extract traces around each event
         def extractor(ev):
@@ -27,7 +66,7 @@ class EpochReducer(BaseEstimator,TransformerMixin):
                 (X.index >= ev['time'])
                 & (X.index < (ev['time'] + ev['duration']))
                 )
-            return X[mask].apply(self.method,axis=0)
+            return X[mask].apply(self.func,axis=0)
 
         # do the extraction
         tensor = [extractor(ev) for _,ev in X.iterrows()]
