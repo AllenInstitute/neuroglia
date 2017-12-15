16cecc9238fbaf7d46d05da319cd2cc3281fc30f
Justin Kiggins <justink@alleninstitute.org>
2017-12-15 13:52:41 -0800

    add 3.6

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setuptools.setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
