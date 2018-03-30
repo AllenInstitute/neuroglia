8077bf949609c20f3c0248f8c759dbbca4abf9c5
Justin Kiggins <justink@alleninstitute.org>
2018-03-29 21:18:32 -0700

    isolate functions

--- neuroglia/datasets/synthetic_calcium.py
+++ neuroglia/datasets/synthetic_calcium.py
@@ -1,7 +1,46 @@
 import numpy as np
 
 
-def gen_data(g=[.95], sn=.3, T=3000, framerate=30, firerate=.5, b=0, N=20, seed=13):
+def gen_random_spikes(N, T, firerate, framerate, seed=None):
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    true_spikes = np.random.rand(N, T) < firerate / float(framerate)
+    return true_spikes
+
+
+def gen_sinusoidal_spikes(N, T, firerate, framerate, seed=None):
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    true_spikes = np.random.rand(N, T) < firerate / float(framerate) * \
+        np.sin(np.arange(T) // 50)**3 * 4
+
+    return true_spikes
+
+def make_calcium(true_spikes, g):
+
+    truth = true_spikes.astype(float)
+
+    for i in range(2, truth.shape[1]):
+        if len(g) == 2:
+            truth[:, i] += g[0] * truth[:, i - 1] + g[1] * truth[:, i - 2]
+        else:
+            truth[:, i] += g[0] * truth[:, i - 1]
+
+    return truth
+
+
+def add_noise(truth, b, sn):
+
+    noise = sn * np.random.randn(*truth.shape)
+
+    return b + truth + noise
+
+
+def gen_data(g=[.95], sn=.3, T=3000, framerate=30, firerate=.5, b=0, N=20, seed=None):
     """
     Generate data from homogenous Poisson Process
 
@@ -34,20 +73,31 @@ def gen_data(g=[.95], sn=.3, T=3000, framerate=30, firerate=.5, b=0, N=20, seed=
         Spike trains.
     """
 
-    np.random.seed(seed)
-    Y = np.zeros((N, T))
-    trueSpikes = np.random.rand(N, T) < firerate / float(framerate)
-    truth = trueSpikes.astype(float)
-    for i in range(2, T):
-        if len(g) == 2:
-            truth[:, i] += g[0] * truth[:, i - 1] + g[1] * truth[:, i - 2]
-        else:
-            truth[:, i] += g[0] * truth[:, i - 1]
-    Y = b + truth + sn * np.random.randn(N, T)
-    return Y, truth, trueSpikes
+    if seed is not None:
+        np.random.seed(seed)
+
+    true_spikes = gen_random_spikes(
+        N=N,
+        T=T,
+        firerate=firerate,
+        framerate=framerate,
+    )
+    true_calcium = make_calcium(true_spikes, g)
+    observed = add_noise(true_calcium, b, sn)
+
+    return observed, true_calcium, true_spikes
 
 
-def gen_sinusoidal_data(g=[.95], sn=.3, T=3000, framerate=30, firerate=.5, b=0, N=20, seed=13):
+def gen_sinusoidal_data(
+    g=(.95,),
+    sn=.3,
+    T=3000,
+    framerate=30,
+    firerate=.5,
+    b=0,
+    N=20,
+    seed=None,
+):
     """
     Generate data from inhomogenous Poisson Process with sinusoidal instantaneous activity
 
@@ -80,15 +130,44 @@ def gen_sinusoidal_data(g=[.95], sn=.3, T=3000, framerate=30, firerate=.5, b=0, 
         Spike trains.
     """
 
-    np.random.seed(seed)
-    Y = np.zeros((N, T))
-    trueSpikes = np.random.rand(N, T) < firerate / float(framerate) * \
-        np.sin(np.arange(T) // 50)**3 * 4
-    truth = trueSpikes.astype(float)
-    for i in range(2, T):
-        if len(g) == 2:
-            truth[:, i] += g[0] * truth[:, i - 1] + g[1] * truth[:, i - 2]
-        else:
-            truth[:, i] += g[0] * truth[:, i - 1]
-    Y = b + truth + sn * np.random.randn(N, T)
-    return Y, truth, trueSpikes
+    if seed is not None:
+        np.random.seed(seed)
+
+    true_spikes = gen_sinusoidal_spikes(
+        N=N,
+        T=T,
+        firerate=firerate,
+        framerate=framerate,
+    )
+    true_calcium = make_calcium(true_spikes, g)
+    observed = add_noise(true_calcium, b, sn)
+
+    return observed, true_calcium, true_spikes
+
+
+def make_calcium_traces(
+    n_neurons=10,
+    duration=60,
+    sampling_rate=30,
+    correlated=True,
+):
+
+    gen_params = dict(
+        g=[.95],
+        sn=.3,
+        T=sampling_rate*duration,
+        framerate=sampling_rate,
+        firerate=.5,
+        b=0,
+        N=n_neurons,
+        seed=13,
+    )
+
+    if correlated:
+        make_traces = gen_sinusoidal_data
+    else:
+        make_traces = gen_data
+
+    traces, _, spikes = make_traces(**gen_params)
+
+    return traces, spikes
