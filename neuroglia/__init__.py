06509fab0dfe1cf0fa54bb06cc361d1d455bec5f
mochic808 <chrism@alleninstitute.org>
2018-04-16 14:27:15 -0700

    Bump version: 0.2.8 → 0.2.9

--- neuroglia/__init__.py
+++ neuroglia/__init__.py
@@ -1,6 +1,6 @@
 """neuroglia - pipelines for transformations for neurophysiology data"""
 
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 __author__ = 'Justin Kiggins <justink@alleninstitute.org>'
 __all__ = []
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy as np
 
 setuptools.setup(
     name="neuroglia",
-    version="0.2.8",
+    version="0.2.9",
     url="https://github.com/AllenInstitute/neuroglia",
 
     author="Justin Kiggins",
