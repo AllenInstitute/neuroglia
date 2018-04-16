c5edd1b43dcef5758f9d15fa4271faa007bd9902
mochic808 <chrism@alleninstitute.org>
2018-04-16 11:47:12 -0700

    Bump version: 0.2.6 → 0.2.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy as np
 
 setuptools.setup(
     name="neuroglia",
-    version="0.2.6",
+    version="0.2.7",
     url="https://github.com/AllenInstitute/neuroglia",
 
     author="Justin Kiggins",
