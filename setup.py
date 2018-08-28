920c5d2facf653dcc7c23169745955ff8b6bede5
Justin Kiggins <justin.kiggins@gmail.com>
2018-08-27 21:07:30 -0700

    Bump version: 0.2.9 → 0.2.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy as np
 
 setuptools.setup(
     name="neuroglia",
-    version="0.2.9",
+    version="0.2.10",
     url="https://github.com/AllenInstitute/neuroglia",
 
     author="Justin Kiggins",
